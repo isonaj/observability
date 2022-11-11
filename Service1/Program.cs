a1c6c7e1d174a58370f9887ce389101ae9de4b90
Anthony Ison <isonaj@gmail.com>
2022-11-11 19:25:04 +1000

    publish trace to console

--- Service1/Program.cs
+++ Service1/Program.cs
@@ -1,6 +1,25 @@
 using System.Diagnostics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+var serviceName = "Service1";
+var serviceVersion = "1.0.0";
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure important OpenTelemetry settings, the console exporter, and instrumentation library
+builder.Services.AddOpenTelemetryTracing(tracerProviderBuilder =>
+{
+    tracerProviderBuilder
+        .AddConsoleExporter()
+        .AddSource(serviceName)
+        .SetResourceBuilder(
+            ResourceBuilder.CreateDefault()
+                .AddService(serviceName: serviceName, serviceVersion: serviceVersion))
+        .AddHttpClientInstrumentation()
+        .AddAspNetCoreInstrumentation();
+});
+
 var app = builder.Build();
 
 var ActivitySource = new ActivitySource("Service1");
